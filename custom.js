bc7802a64000cd541a3f2120f1076d2a63339a9e
Wanderley Mayhé Junior <wanderley.junior@farfetch.com>
2020-07-14 11:40:09 +0100

    fixes

--- custom.js
+++ custom.js
@@ -13,30 +13,39 @@ if (typeof (element) != 'undefined' && element != null) {
     // replace mstest plugin
     element.innerHTML = element.innerHTML.replace(/\[MSTEST-PLUGIN.*/gm, '\n');
 
-    // replace empty tags
-    element.innerHTML = element.innerHTML.replace(/<[^/>]+>[ \n\r\t]*<\/[^>]+>/gm, '\n');
-
     // other bash noises
-    element.innerHTML = element.innerHTML.replace(/\+ tree.*/gm, '\n').replace(/\+ printenv.*/gm, '\n');
-    element.innerHTML = element.innerHTML.replace(/^\>\d{2}.\d{2}\.\d{4}\|\d{2}:\d{2}:\d{2}\.\d{3}\s*$/gm, '>\n');
+    element.innerHTML = element.innerHTML.replace(/\+ tree.*/gm, '\n').replace(/\+ printenv.*/gm, '\n').replace(/\+ salt-call.*/gm, '\n').replace(/\nlocal:\n    None.*/gm, '\n').replace(/\nlocal:\n    True.*/gm, '\n').replace(/(\[\d{1,2}m){2,}/g, '');
+    element.innerHTML = element.innerHTML.replace(/>\d{2}.\d{2}\.\d{4}\|\d{2}:\d{2}:\d{2}\.\d{3}\s*$/gm, '>\n');
 
-    // replace duplicate new lines
-    element.innerHTML = element.innerHTML.replace(/^\s*[\r\n]/gm, '\n');
+    
 
     // normalize new lines
     element.innerHTML = element.innerHTML.replace(/(\n\s*){2,}/g, '\n');
+	
+	// replace empty tags
+    element.innerHTML = element.innerHTML.replace(/<[^/>]+>[ \n\r\t]*<\/[^>]+>/gm, '\n');
+	
+	// replace duplicate new lines
+    element.innerHTML = element.innerHTML.replace(/^\s*[\r\n]/gm, '\n');
 
     // colorize
+	element.innerHTML = element.innerHTML.replace(/\Finished: FAILURE.*/gm, '<span style="color:red">Finished: FAILURE</span>');
+	element.innerHTML = element.innerHTML.replace(/\hudson.remoting.ProxyException.*/gm, '<span style="color:red">hudson.remoting.ProxyException</span>');
+	
+	
     for (const line of element.querySelectorAll('span')) {
-        if (line.textContent.includes('dotnet') || line.textContent.includes('Test run') || line.textContent.includes('Finished: SUCCESS')) { line.style.color = 'green'; }
+		var l = line.textContent.toLowerCase();
+        if ((l.includes('dotnet') && !l.includes('error:')) || l.includes('test run') || l.includes('finished: success')) { line.style.color = 'lime'; }
     }
 
     for (const line of element.querySelectorAll('span')) {
-        if (line.textContent.includes('error') || line.textContent.includes('failed') || line.textContent.includes('Finished: FAILURE')) { line.style.color = 'red'; }
+		var l = line.textContent.toLowerCase();
+        if (l.includes('error') || l.includes('error:') || l.includes('failed')) { line.style.color = 'red'; }
     }
 
     for (const line of element.querySelectorAll('span')) {
-        if (line.textContent.includes('warning')) { line.style.color = 'yellow'; }
+		var l = line.textContent.toLowerCase();
+        if (l.includes('warning')) { line.style.color = 'yellow'; }
     }
 }
 else {
