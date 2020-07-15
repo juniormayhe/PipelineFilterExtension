b5f1746a395a457271fd8204bcef017c2210fe10
Wanderley Mayhé Junior <wanderley.junior@farfetch.com>
2020-07-15 12:20:32 +0100

    permission fix

--- popup.js
+++ popup.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     var btn = document.getElementById('filterNow');
     btn.addEventListener('click', function () {
         
-        chrome.tabs.executeScript(null, {file: 'custom.js'});
+        var activeTab = null; // when not defined assumes active tab  as default
+		chrome.tabs.executeScript(null, {file: 'custom.js'});
 
         btn.innerText = 'Done!';
-        window.setTimeout(function(){ btn.innerText = 'Filter!';}, 1000);
+        window.setTimeout(function(){ btn.innerText = 'Filter Log!';}, 1000);
 		
 		window.setTimeout(function(){ window.close();}, 500);
 
